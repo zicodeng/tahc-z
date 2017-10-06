15f3dc3ea63f6c16c0516c0ff2d588ee54469afd
Zico Deng <zicodeng@gmail.com>
2017-10-06 00:01:33 -0700

    Implement the main function.

--- servers/gateway/main.go
+++ servers/gateway/main.go
@@ -1,17 +1,30 @@
 package main
 
+import (
+	"github.com/zicodeng/info-344/challenges-zicodeng/servers/gateway/handlers"
+	"net/http"
+)
+
 //main is the main entry point for the server
 func main() {
-	/* TODO: add code to do the following
-	- Read the ADDR environment variable to get the address
-	  the server should listen on. If empty, default to ":80"
-	- Create a new mux for the web server.
-	- Tell the mux to call your handlers.SummaryHandler function
-	  when the "/v1/summary" URL path is requested.
-	- Start a web server listening on the address you read from
-	  the environment variable, using the mux you created as
-	  the root handler. Use log.Fatal() to report any errors
-	  that occur when trying to start the web server.
-	*/
+	// Read the ADDR environment variable to get the address
+	// the server should listen on. If empty, default to ":80"
+	addr := os.Getenv("ADDR")
+	if len(addr) == 0 {
+		addr := ":80"
+	}
 
+	// Create a new mux for the web server.
+	mux := http.NewServeMux()
+
+	// Tell the mux to call your handlers.SummaryHandler function
+	// when the "/v1/summary" URL path is requested.
+	mux.Handle("/v1/summary", handlers.SummaryHandler)
+
+	// Start a web server listening on the address you read from
+	// the environment variable, using the mux you created as
+	// the root handler. Use log.Fatal() to report any errors
+	// that occur when trying to start the web server.
+	log.Printf("Server is listening at http://%s\n", addr)
+	log.Fatal(http.ListenAndServe(addr, mux))
 }
