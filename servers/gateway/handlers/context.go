9aad9740336b09df8da44be40db51ed092515447
Zico Deng <zicodeng@gmail.com>
2017-10-28 12:17:04 -0700

    Implement handler context struct

--- servers/gateway/handlers/context.go
+++ servers/gateway/handlers/context.go
@@ -1,8 +1,37 @@
 package handlers
 
-//TODO: define a handler context struct that
-//will be a receiver on any of your HTTP
-//handler functions that need access to
-//globals, such as the key used for signing
-//and verifying SessionIDs, the session store
-//and the user store
+import (
+	"github.com/info344-a17/challenges-zicodeng/servers/gateway/models/users"
+	"github.com/info344-a17/challenges-zicodeng/servers/gateway/sessions"
+	"gopkg.in/mgo.v2"
+)
+
+// HandlerContext will be a receiver on any of your HTTP
+// handler functions that need access to
+// globals, such as the key used for signing
+// and verifying SessionIDs, the session store
+// and the user store.
+type HandlerContext struct {
+	SigningKey   string
+	SessionStore *sessions.Store
+	UserStore    *users.Store
+}
+
+// NewHandlerContext constructs a new HanderContext,
+// ensuring that the dependencies are valid values.
+func NewHandlerContext(signingKey string, sessionStore *sessions.Store, userStore *users.Store) *HandlerContext {
+
+	if len(signingKey) == 0 {
+		panic("signing key has length of zero")
+	}
+
+	if sessionStore == nil {
+		panic("nil session store")
+	}
+
+	if userStore == nil {
+		panic("nil user store")
+	}
+
+	return &HandlerContext{signingKey, sessionStore, userStore}
+}
