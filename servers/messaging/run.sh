96ea569401e2ce924a4242c335a473cfcbe77153
Zico Deng <zicodeng@gmail.com>
2017-11-11 20:21:55 -0800

    Add bash script for deploying messaging microservice

---
+++ servers/messaging/run.sh
@@ -0,0 +1,29 @@
+#!usr/bin/env bash
+
+set -e
+
+export MESSAGING_CONTAINER=info-344-messaging
+export APP_NETWORK=appnet
+
+docker pull zicodeng/$MESSAGING_CONTAINER
+
+if [ "$(docker ps -aq --filter name=$MESSAGING_CONTAINER)" ]; then
+    docker rm -f $MESSAGING_CONTAINER
+fi
+
+if [ "$(docker images -q -f dangling=true)" ]; then
+    docker rmi $(docker images -q -f dangling=true)
+fi
+
+docker system prune -f
+
+if ! [ "$(docker network ls | grep $APP_NETWORK)" ]; then
+    docker network create $APP_NETWORK
+fi
+
+docker run \
+-d \
+--name $MESSAGING_CONTAINER \
+--network $APP_NETWORK \
+--restart unless-stopped \
+zicodeng/$MESSAGING_CONTAINER
